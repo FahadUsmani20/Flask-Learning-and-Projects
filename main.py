b62c6755e0e72a4e41b01938460df2021c139cc1
fahadusmani20 <fahad.usmani2020@vitbhopal.ac.in>
2024-05-03 11:56:48 +0530

    POST and GET Methods

--- main.py
+++ main.py
@@ -1,48 +1,35 @@
 # Integrate HTML with Flask
 # HTTP verb GET and POST
 
-# Building URL Dynamically
-#Variable Rules and URL Building
-
 from flask import Flask, redirect, url_for, render_template, request
+
+#render template requires a template folder to work
+#requests is used to get data from pages
 
 app = Flask(__name__)
 
-@app.route('/')
-
+@app.route('/') #Home Page
 def welcome():
-    return render_template('index.html')
+    return render_template('index.html') 
 
-@app.route('/success/<int:score>')
-
-def success(score):
-    return f"Passed! \nThe Person score is {score}"
-
-@app.route('/fail/<int:score>')
-
-def fail(score):
-    return f"Failed! \nThe Person score is {score}"
-
-@app.route('/results/<int:marks>')
-
+@app.route('/results/<int:marks>') #Results Page
 def results(marks):
-    result = ""
+    res = ""
 
     if marks < 50:
-        result = 'fail'
+        res = 'Failed'
     else:
-        result = 'success'
-
-    return redirect(url_for(result, score = marks)) 
-    #Result is basically a URL
-    #score is the variable we have defined
+        res = 'Passed'
+    
+    return render_template('result.html', grade = res)
+    #grade = res // grade is the value in result.html 
 
 # Result Checker HTML page
 @app.route('/submit', methods = ['POST', 'GET'])
 def submit():
-    total_score = 0
 
     if request.method == 'POST':
+        
         science = float(request.form['science'])
         maths = float(request.form['maths'])
         c = float(request.form['c'])
@@ -50,13 +37,8 @@ def submit():
 
         total_score = (science+maths+c+datascience)/4
 
-    res = ""
-    if total_score >= 50:
-        res = "success"
-    else:
-        res = 'fail'
 
-    return redirect(url_for(res, score = total_score))
+    return redirect(url_for("results", marks = total_score))
 
 
 if __name__ == '__main__':
